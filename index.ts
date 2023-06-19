0f99f46550d6bfb0e98cf3582893076a2879b999
garvityadav07 <garvityadav07051999@gmail.com>
2023-06-19 14:05:21 +0530

    update

--- index.ts
+++ index.ts
@@ -9,13 +9,13 @@ import {
   pattern8,
   pattern9,
 } from "./patterns";
-import { sortingEx_1, bubbleSort, insertionSort } from "./sorting";
+import { selectionSort, bubbleSort, insertionSort } from "./sorting";
 const patterns = (pattern: any, number: number) => {
   return pattern(number);
 };
 
 // patterns(pattern9, 5);
 
-//sortingEx_1(6, [12, 6, 25, 78, 100, 1]);
-// bubbleSort(6, [5, 4, 3, 2, 1, 0]);
+selectionSort(6, [12, 6, 25, 78, 100, 1]);
+bubbleSort(6, [5, 4, 3, 2, 1, 0]);
 insertionSort(6, [5, 4, 3, 2, 1, 0]);
--- sorting/bubble_sort.ts
+++ sorting/bubble_sort.ts
@@ -1,7 +1,7 @@
 export function bubbleSort(n: number, arr: number[]) {
-  let bestCase = 0;
-  for (let i = 0; i < n - 1; i++) {
-    for (let j = 0; j < n - i; j++) {
+  for (let i = n - 1; i >= 1; i--) {
+    let bestCase = 0;
+    for (let j = 0; j <= i - 1; j++) {
       if (arr[j] > arr[j + 1]) {
         let temp = arr[j];
         arr[j] = arr[j + 1];
